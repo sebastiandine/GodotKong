b9c71a684b989b0f984d6069e4ed82857bb7fa7c
Sebastian Dine <none>
2020-09-17 00:00:00 +0000

    Initial commit - Player movement & basic platform.

---
+++ Scripts/GlobalData.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# ----------------------------------------------------
+# This script contains global data for the whole game.
+# ----------------------------------------------------
+
+var level = 1
+
+func _ready():
+	pass 
