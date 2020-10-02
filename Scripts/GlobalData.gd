c8f2dd9b59d0c09451fde810f4722c193ad00e3e
Sebastian Dine <none>
2020-10-02 00:00:00 +0000

    Bonus score added.

--- Scripts/GlobalData.gd
+++ Scripts/GlobalData.gd
@@ -4,9 +4,11 @@ extends Node
 # This script contains global data for the whole game.
 # ----------------------------------------------------
 
-var level = 1
-var lives = 3
-var score = 0
+var level: int = 1
+var lives: int = 3
+var score: int = 0
+
+var bonus_score: int = 50000
 
 func _ready():
 	pass 
